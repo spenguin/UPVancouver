6a864aaa68936a06d3ebaeb41d4ade8f0fbc86a6
John Anderson <info@soaringpenguin.com>
2025-02-24 13:23:31 -0800

    Correct code to maintain cart properly.

--- includes/shortcodes/upv_cart.php
+++ includes/shortcodes/upv_cart.php
@@ -18,6 +18,7 @@ function upv_cart()
         { 
             if( $t->quantity == 0 ) continue;
             $args  = [
+                'product_id'=> $t->ticketId,
                 'quantity'  => $t->quantity,
                 'date'      => $selectedPerformance->post_title,
                 'time'      => $performanceMeta['performance_time'][0],
@@ -25,7 +26,7 @@ function upv_cart()
                 'misha_custom_price' => $t->charge,
                 'name'      => $t->name
             ];
-            $_SESSION['cart'][$t->ticketid] = $args;
+            $_SESSION['cart'][] = $args;
         }
     }
 
@@ -33,11 +34,11 @@ function upv_cart()
         // Do something with the donation
         $donationProductId  = getDonationProduct(); 
         $_SESSION['cart'][$donationProductId] = [
-            'quantity'  => 1,
+            'product_id'    => $donationProductId,
+            'quantity'      => 1,
             'misha_custom_price'    => $_POST['donation'],
-            'name'      => 'Donation'
+            'name'          => 'Donation'
         ];
-        // WC()->cart->add_to_cart($donationProductId, 1, 0, [], ['misha_custom_price' => $_POST['donation']] );
     }
 
     if( isset($_GET['del']) ){
@@ -63,46 +64,30 @@ function upv_cart()
                 <tbody>
                     <?php
                         $orderTotal = 0;
-                        foreach($_SESSION['cart']  as $key => $t ) {
-                            if( $t['quantity'] == 0 ) continue;
-                            $showCharge = $t['quantity'] * $t['misha_custom_price'];
+                        foreach($_SESSION['cart']  as $key => $args ) {
+                            if( $args['quantity'] == 0 ) continue;
+                            $showCharge = $args['quantity'] * $args['misha_custom_price'];
                             $orderTotal += $showCharge;
                             ?>
                             <tr>
                                 <td>
                                     <div class="shopping-cart__delete"><a href="/cart?del=<?php echo $key; ?>" >X</a></div>
                                     <div>
-                                        <?php if( $t['name'] != 'Donation' ): ?>
-                                            <?php echo $t['showTitle']; ?><br />
-                                            <?php echo $t['date']  . ' '  . date("g:i a", strtotime($t['time'])); ?><br />
+                                        <?php if( $args['name'] != 'Donation' ): ?>
+                                            <?php echo $args['showTitle']; ?><br />
+                                            <?php echo $args['date']  . ' '  . date("g:i a", strtotime($args['time'])); ?><br />
                                         <?php endif; ?>
-                                        <?php echo $t['name'] . ' &dollar;' . $t['misha_custom_price']; ?>
+                                        <?php echo $args['name'] . ' &dollar;' . $args['misha_custom_price']; ?>
                                     </div>
                                 </td>
                                 <td>
-                                    <?php echo $t['quantity']; ?>
+                                    <?php echo $args['quantity']; ?>
                                 </td>
                                 <td>
                                     &dollar;<?php echo $showCharge; ?>
                                 </td>
                             </tr>
                     <?php    } ?>
-                    <!-- <tr>
-                        <td class="cart-total__label">Subtotal:</td>
-                        <td>&nbsp;</td>
-                        <td class="cart-total">&dollar;<?php echo $orderTotal; ?></td>
-                    </tr> -->
-                    <!-- <tr>
-                        <td class="cart-total__label">&nbsp;</td>
-                        <td>Would you like to make a <a href="/donate">Donation</a>?</td>
-                        <td class="cart-total">
-                            <form>
-                                <input type="text" name="donation">
-                                
-                            </form>
-
-                        </td>
-                    </tr> -->
                     <tr>
                         <td class="cart-total__label">Total:</td>
                         <td>&nbsp;</td>
